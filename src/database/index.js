55c44f7f8191290405f0ef169c8db9bee9a99e1c
ThayPedroso <44008476+ThayPedroso@users.noreply.github.com>
2020-02-12 12:53:05 -0300

    hideDatabaseKey
    Using dotenv to hide database key

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb+srv://thaypedroso:omnistack2020@cluster0-xk1im.mongodb.net/finance?retryWrites=true&w=majority', {
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const name = process.env.DB_NAME
+
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-xk1im.mongodb.net/${name}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
